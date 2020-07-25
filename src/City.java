44615c2b851365f1ac6f8a7ab13c915d0f5813a5
barakfinnegan <BJFinn98@vt.edu>
2020-07-25 05:39:02 -0400

    Created the City object. Decided to not implement comparable, for we
    won't need to compare Cities in the BinTree

---
+++ src/City.java
@@ -0,0 +1,107 @@
+/**
+ * The underlying data structure that will represent our cities
+ * 
+ * @author Barak Finnegan (bjfinn98)
+ * @version 2020.07.25
+ */
+public class City {
+
+    // Setting up private vars
+    // Final for there is no edit option for cities in project spec.
+    private final String cityName;
+    private final int x;
+    private final int y;
+
+    /**
+     * This is the version 1 of the constructor for the City class.
+     * 
+     * @param name
+     *            name of city
+     * @param x
+     *            x coordinate of city
+     * @param y
+     *            y coordinate of city
+     */
+    public City(String name, int x, int y) {
+        this.cityName = name;
+        this.x = x;
+        this.y = y;
+    }
+
+
+    /**
+     * This is the version 2 of the constructor for the City class.
+     * 
+     * @param name
+     *            name of city
+     * @param x
+     *            x coordinate of city
+     * @param y
+     *            y coordinate of city
+     */
+    public City(int x, int y, String name) {
+        this(name, x, y);
+    }
+
+
+    /**
+     * Needs the variables to be readable, but not writable
+     * 
+     * @return x coordinate of city
+     */
+    public int getX() {
+        return x;
+    }
+
+
+    /**
+     * Needs the variables to be readable, but not writable
+     * 
+     * @return y coordinate of city
+     */
+    public int getY() {
+        return y;
+    }
+
+
+    /**
+     * Needs the variables to be readable, but not writable
+     * 
+     * @return name of city
+     */
+    public String getName() {
+        return cityName;
+    }
+
+
+    /**
+     * The standard equals method for the City class
+     * 
+     * @return boolean based on whether the object in parameter is equal to this
+     *         object
+     * @param object
+     *            the object to be compare to this object
+     */
+    public boolean equals(Object other) {
+        // In order to invoke this method this object cannot be null
+        if (other == null) {
+            return false;
+        }
+        if (!(other instanceof City)) {
+            return false;
+        }
+        City castedOther = (City)other;
+        return (this.getX() == castedOther.getX() && this.getY() == castedOther
+            .getY() && this.getName().contentEquals(castedOther.getName()));
+    }
+
+
+    /**
+     * A standard toString method for the City object.
+     * 
+     * @return String a String representation of the city obj.
+     */
+    public String toString() {
+        return String.format("%s %d %d", cityName, x, y);
+    }
+}
---
+++ src/CityTest.java
@@ -0,0 +1,85 @@
+import student.TestCase;
+
+/**
+ * The unit testing class for the City Object
+ * 
+ * @author Barak Finnegan (bjfinn98)
+ * @version 2020.07.25
+ */
+public class CityTest extends TestCase {
+
+    // setting up the local variables
+    private City test1;
+    private City test2;
+
+    /**
+     * This is the setUp method for the City Testing suite.
+     */
+    public void setUp() {
+        test1 = new City("Hello_town", 500, 200);
+        test2 = new City(250, 420, "Apple_town");
+    }
+
+
+    /**
+     * This will test the getX method
+     */
+    public void testGetX() {
+        assertEquals(500, test1.getX());
+        assertEquals(250, test2.getX());
+    }
+
+
+    /**
+     * This will test the getY method
+     */
+    public void testGetY() {
+        assertEquals(200, test1.getY());
+        assertEquals(420, test2.getY());
+    }
+
+
+    /**
+     * This will test the getName method
+     */
+    public void testGetName() {
+        assertEquals("Hello_town", test1.getName());
+        assertEquals("Apple_town", test2.getName());
+    }
+
+
+    /**
+     * This will test the equals method
+     */
+    public void testGetEquals() {
+        City equaltest1 = null;
+        String equaltest2 = "Hello_town";
+        City equaltest3 = new City("Hello_town", 0, 0);
+        City equaltest4 = new City("abc", 500, 0);
+        City equaltest5 = new City("abc", 0, 200);
+        City equaltest6 = new City("Hello_town", 500, 0);
+        City equaltest7 = new City("Hello_town", 0, 200);
+        City equaltest8 = new City("abc", 500, 200);
+        City equaltest9 = new City("Hello_town", 500, 200);
+        assertFalse(test1.equals(equaltest1));
+        assertFalse(test1.equals(equaltest2));
+        assertFalse(test1.equals(equaltest3));
+        assertFalse(test1.equals(equaltest4));
+        assertFalse(test1.equals(equaltest5));
+        assertFalse(test1.equals(equaltest6));
+        assertFalse(test1.equals(equaltest7));
+        assertFalse(test1.equals(equaltest8));
+        assertTrue(test1.equals(equaltest9));
+        assertFalse(test1.equals(test2));
+    }
+
+
+    /**
+     * This will test the toString method
+     */
+    public void testToString() {
+        assertEquals("Hello_town 500 200", test1.toString());
+        assertEquals("Apple_town 250 420", test2.toString());
+    }
+
+}
