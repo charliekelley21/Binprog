28362086f7a5754dab06838779faefd229173ba4
barakfinnegan <BJFinn98@vt.edu>
2020-07-26 01:04:48 -0400

    Haven't debugged this yet. Logic worked out, but not sure if functional.
    Should get it done by tomorrow.

--- src/CityTree.java
+++ src/CityTree.java
@@ -2,7 +2,7 @@
  * The underlying data structure that handle all our logical commands
  * recursively.
  * 
- * Even levels wil split on x, while odd levels will split on y.
+ * Even levels will split on x, while odd levels will split on y.
  * 
  * @author Charlie Kelley (charlk21)
  * @author Barak Finnegan (bjfinn98)
@@ -13,6 +13,7 @@ public class CityTree {
     // Setting up local vars.
     private BaseNode<City> root;
     private int size;
+    private final int WORLDSIZE = 1024;
 
     /**
      * This is the default zero information instantiation of CityTree
@@ -33,6 +34,139 @@ public class CityTree {
 
 
     /**
+     * This is the insert method for the CityTree.
+     * 
+     * @param newCity
+     *            new city to be inserted
+     * @return boolean telling if the insertion was performed.
+     */
+    public boolean insert(City newCity) {
+        if (newCity.getX() >= WORLDSIZE || newCity.getX() < 0 || newCity
+            .getY() >= WORLDSIZE || newCity.getY() < 0) {
+            return false;
+        }
+        // the split boolean carries info as to which axis to cut in half.
+        // we split along the x axis first, the ints will remember the slice
+        // location.
+        boolean answer = insert(root, null, newCity, false, WORLDSIZE / 2,
+            WORLDSIZE / 2, WORLDSIZE / 4);
+        if (answer) {
+            size++;
+            return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * Recursive helper method for the insert method
+     * 
+     * @param root
+     *            The current root node of the subtree
+     * @param last
+     *            The parent node of the current node
+     * @param newCity
+     *            the new city to be inserted
+     * @param splitY
+     *            boolean the indicator whether to split on y
+     * @param xcut
+     *            int that remembers where in the subsection we are in for x
+     * @param ycut
+     *            int that remembers where in the subsection we are in for y
+     * @param splitdist
+     *            int that knows how to shift xcut or ycut
+     * @return boolean on whether operation successful or not (won't be if
+     *         coordinates already exist)
+     */
+    private boolean insert(
+        BaseNode<City> root,
+        InternalNode<City> last,
+        City newCity,
+        boolean splitY,
+        int xcut,
+        int ycut,
+        int splitdist) {
+
+        // test if root is leaf
+        if (root.isLeaf() == true) {
+            LeafNode<City> temp = (LeafNode<City>)root;
+            // if flyweight
+            if (temp.value() == null) {
+                temp.setValue(newCity);
+                return true;
+            }
+            else {
+                // we have a filled leaf node
+                // if the filled leaf node has equal coords to newCity return
+                // false if not equal we need to change the leafnode to a
+                // internalnode , give it two leafnode children, and call
+                // insert on both of the colliding nodes.
+                if (temp.value().getX() == newCity.getX() && temp.value()
+                    .getY() == newCity.getY()) {
+                    return false;
+                }
+
+                InternalNode<City> newInternalNode = new InternalNode<City>(
+                    null, new LeafNode<City>(), new LeafNode<City>());
+
+                // this new internal node must be linked to the last parent
+                // node;
+                if (root == last.right()) {
+                    last.setRight(newInternalNode);
+                }
+                else {
+                    last.setLeft(newInternalNode);
+                }
+
+                // reinserting temp.value() which was the old value, and the
+                // newCity value into the new Internal Node
+                boolean first = insert(newInternalNode, last, temp.value(),
+                    splitY, xcut, ycut, splitdist);
+                boolean second = insert(newInternalNode, last, newCity, splitY,
+                    xcut, ycut, splitdist);
+                return first && second;
+            }
+
+        }
+        else {
+            // the root is an internal node, and we need to find out which way
+            // to traverse the tree
+            InternalNode<City> temp = (InternalNode<City>)root;
+            if (splitY) {
+                if (newCity.getY() >= ycut) {
+                    // shift ycut up, change to xcut, on y cuts we halve the
+                    // split distance also
+                    // we also got to root's right, but first we need to case it
+                    return insert(temp.right(), temp, newCity, false, xcut, ycut
+                        + splitdist, splitdist / 2);
+                }
+                else {
+                    // The same as before, only going left and subtracting
+                    // splitdist from ycut
+                    return insert(temp.left(), temp, newCity, false, xcut, ycut
+                        - splitdist, splitdist / 2);
+                }
+            }
+            else {
+                if (newCity.getX() >= xcut) {
+                    // shift ycut up, change to xcut, on y cuts we halve the
+                    // split distance also
+                    // we also got to root's right, but first we need to case it
+                    return insert(temp.right(), temp, newCity, true, xcut
+                        + splitdist, ycut, splitdist);
+                }
+                else {
+                    // The same as before, only going left and subtracting
+                    // splitdist from ycut
+                    return insert(temp.left(), temp, newCity, true, xcut
+                        - splitdist, ycut, splitdist);
+                }
+            }
+        }
+    }
+
+
+    /**
      * Gets the number of items in the CityTree
      * 
      * @return int of number of items in CityTree
@@ -49,7 +183,8 @@ public class CityTree {
         root = new LeafNode<City>();
         size = 0;
     }
-    
+
+
     /**
      * Prints the contents of CityTree in pre-order
      */
--- src/InternalNode.java
+++ src/InternalNode.java
@@ -5,50 +5,73 @@
  * @version 2020.07.25
  */
 public class InternalNode<E> implements BaseNode<E> {
-    private E v;    //might not be needed
+    private E v; // might not be needed
     private BaseNode<E> left;
     private BaseNode<E> right;
-    
+
     InternalNode(E newValue, BaseNode<E> l, BaseNode<E> r) {
         v = newValue;
         left = l;
         right = r;
     }
-    
+
+
     public boolean isLeaf() {
         return false;
     }
-    
-    public String printNode(BaseNode<E> rt, int level, int x, int y, int w, int h) {
-        if (rt == null) return "";
-        
-        String node = new String(new char[level*2]).replace("\0", " ");
+
+
+    public String printNode(
+        BaseNode<E> rt,
+        int level,
+        int x,
+        int y,
+        int w,
+        int h) {
+        if (rt == null)
+            return "";
+
+        String node = new String(new char[level * 2]).replace("\0", " ");
         node += String.format("I, %d, %d, %d, %d\n", x, y, w, h);
-        
-        if (level%2 == 0) { //split the X grid
-            return node + printNode(left, level + 1, x, y, w / 2, h) 
+
+        if (level % 2 == 0) { // split the X grid
+            return node + printNode(left, level + 1, x, y, w / 2, h)
                 + printNode(left, level + 1, x + (w / 2), y, w / 2, h);
         }
-        else { //split the Y grid
-            return node + printNode(left, level + 1, x, y, w, h / 2) 
-            + printNode(left, level + 1, x, y + (h / 2), w, h / 2);
+        else { // split the Y grid
+            return node + printNode(left, level + 1, x, y, w, h / 2)
+                + printNode(left, level + 1, x, y + (h / 2), w, h / 2);
         }
     }
-    
+
+
     public E value() {
         return v;
     }
-    
+
+
     public void setValue(E it) {
         v = it;
     }
-    
+
+
     public BaseNode<E> left() {
         return left;
     }
-    
+
+
     public BaseNode<E> right() {
         return right;
     }
-    
+
+
+    public void setLeft(BaseNode<E> newLeft) {
+        this.left = newLeft;
+    }
+
+
+    public void setRight(BaseNode<E> newRight) {
+        this.right = newRight;
+    }
+
 }
