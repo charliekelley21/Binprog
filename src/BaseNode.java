10b13bb97392e88204eb3609251772142373db57
charliekelley21 <charlk21@vt.edu>
2020-07-26 23:17:06 -0400

    Update BaseNode.java

--- src/BaseNode.java
+++ src/BaseNode.java
@@ -1,5 +1,12 @@
-package src;
 
-public interface BaseNode {
-
+/**
+ * The interface that our two Node types will be built off of.
+ * 
+ * @author Charlie Kelley (charlk21)
+ * @version 2020.07.25
+ * 
+ * @param <E> type to be store in BaseNode
+ */
+public interface BaseNode<E> {
+    public boolean isLeaf();
 }
