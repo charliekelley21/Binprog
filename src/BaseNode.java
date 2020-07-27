fe50a5ef1e559372edc8e628e7cd3c2e79ec188e
charliekelley21 <charlk21@vt.edu>
2020-07-26 23:16:20 -0400

    getting weird error in ecipse, just trying to fix

--- src/BaseNode.java
+++ src/BaseNode.java
@@ -1,10 +1,5 @@
+package src;
 
-/**
- * The interface that our two Node types will be built off of.
- * 
- * @author Charlie Kelley (charlk21)
- * @version 2020.07.25
- */
-public interface BaseNode<E> {
-    public boolean isLeaf();
+public interface BaseNode {
+
 }
